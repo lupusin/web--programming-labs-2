b8bd6f4eff88600517489521afd53b093516c355
Лапшин Егор <egorkafghj@gmail.com>
2024-09-07 12:50:58 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,5 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
-    
+    return """
+    <!doctype html?
+    <html>
+        <head>
+            <title>Лапшин Егор Вячеславович, Лабороторная 1 </title>
+        </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лаборотрная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Егор Лапшин , ФБИ-23, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""    
