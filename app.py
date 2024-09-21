595e1943fac67097a4407debe9640c36ba146004
Лапшин Егор <egorkafghj@gmail.com>
2024-09-21 14:39:13 +0700

    Книги

--- app.py
+++ app.py
@@ -207,4 +207,16 @@ def example():
         {'name':'апельсины','price':80},
         {'name':'мандарины','price':95},
         {'name':'манго','price':32}]
-    return render_template('example.html',group=group,kyrc=kyrc,number=number,name=name,fruits=fruits)
+    books = [
+        {'name':'Гроза','author':'Александр Островский','janr':'Пьеса','kolvo':'60'},
+        {'name':'Морфий','author':'Михаил Булгаков','janr':'Русская классика','kolvo':'31'},
+        {'name':'Капитанская дочка','author':'Александр Пушкин','janr':'Русская классика','kolvo':'130'},
+        {'name':'Жена на один год','author':'Марья Коваленко','janr':'Роман','kolvo':'140'},
+        {'name':'На дне','author':'Максим Горький','janr':'Пьеса','kolvo':'70'},
+        {'name':'Скандал на драконьем факультете','author':'Тальяна Орлова','janr':'Любовное фентези','kolvo':'250'},
+        {'name':'Братья Карамазовы','author':'Федор Достаевский','janr':'Русская классика','kolvo':'1140'},
+        {'name':'Люди сороковой мили','author':'Джек Лондон','janr':'Рассказы','kolvo':'70'},
+        {'name':'Искажающие реальность. Книга 1. Обратный отсчет','author':'Михаил Атаманов','janr':'Боевая фантастика','kolvo':'392'},
+        {'name':'Граф Монте-Кристо','author':'Александр Дюма','janr':'Исторические приключения','kolvo':'1500'}
+    ]
+    return render_template('example.html',group=group,kyrc=kyrc,number=number,name=name,fruits=fruits,books=books)
