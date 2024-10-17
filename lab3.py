dd55845574ba7e9f24e9ec412de4ea12ad78b742
Егор Лапшин <egorkafghj@gmail.com>
2024-10-17 21:07:44 +0700

    Железнодорожный билет

--- lab3.py
+++ lab3.py
@@ -4,8 +4,13 @@ lab3 = Blueprint('lab3',__name__)
 @lab3.route('/lab3/')
 def lab():
     name = request.cookies.get('name')
+    if name==None:
+        name = " Геральд"
     name_color = request.cookies.get('name_color')
-    return render_template('lab3/lab3.html',name=name,name_color=name_color)
+    age = request.cookies.get('age')
+    if age==None:
+        age = " неизвестно"
+    return render_template('lab3/lab3.html',name=name,name_color=name_color,age=age)
 
 
 @lab3.route('/lab3/cookie')
@@ -81,3 +86,49 @@ def settings():
     color=request.cookies.get('color')
     resp = make_response(render_template('lab3/settings.html',color=color))
     return resp
+
+
+@lab3.route('/lab3/ticket_form')
+def ticket():
+    return render_template('lab3/ticket_form.html')
+
+@lab3.route('/lab3/calculate', methods=['POST'])
+def calculate():
+    fio = request.form['fio']
+    shelf = request.form['shelf']
+    bedding = 'bedding' in request.form
+    luggage = 'luggage' in request.form
+    age = int(request.form['age'])
+    departure = request.form['departure']
+    destination = request.form['destination']
+    date = request.form['date']
+    insurance = 'insurance' in request.form
+
+    # Цены и расчеты
+    price = 700 if age < 18 else 1000
+
+    if shelf in ['lower', 'lower_side']:
+        price += 100
+    if bedding:
+        price += 75
+    if luggage:
+        price += 250
+    if insurance:
+        price += 150
+
+    ticket_info = {
+        'fio': fio,
+        'shelf': shelf,
+        'bedding': bedding,
+        'luggage': luggage,
+        'age': age,
+        'departure': departure,
+        'destination': destination,
+        'date': date,
+        'insurance': insurance,
+        'price': price,
+        'child_ticket': age < 18
+    }
+
+    return render_template('lab3/ticket.html', ticket=ticket_info)
+
