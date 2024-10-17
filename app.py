ae7fc283712ebcc479afcc0fc6ec0a7350fa8b23
Егор Лапшин <egorkafghj@gmail.com>
2024-10-17 16:11:40 +0700

    Заготовка к третьей лабораторной работе

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask,redirect,url_for,render_template
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 @app.route("/")
 @app.route("/index")
@@ -32,6 +34,7 @@ def menu():
         <h2>Меню</h2>
         <li><a href="/lab1">Лабораторная 1</a></li>
         <li><a href="/lab2">Лабораторная 2</a></li>
+        <li><a href="/lab3">Лабораторная 3</a></li>
         </div>
         <footer>
             &copy; Егор Лапшин , ФБИ-23, 3 курс, 2024
