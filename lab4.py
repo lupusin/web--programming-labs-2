b52221edfaf842ffd98750833ffb99b89237b45b
Егор <egorkafghj@gmail.com>
2024-11-01 20:46:17 +0700

    Валидация

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,request
+from flask import Blueprint,render_template,request, redirect
 lab4 = Blueprint('lab4',__name__)
 
 
@@ -26,75 +26,23 @@ def div():
     except ValueError:
         return render_template('lab4/div-form.html', error='Оба поля должны содержать числа')
 
-@lab4.route('/lab4/sum-form')
-def sum_form():
-    return render_template('lab4/sum-form.html')
+tree_count=0
 
-@lab4.route('/lab4/sum', methods=['POST'])
-def sum_():
-    x1 = request.form.get('x1') or '0'
-    x2 = request.form.get('x2') or '0'
-    try:
-        x1 = int(x1)
-        x2 = int(x2)
-    except ValueError:
-        return render_template('lab4/sum.html', error='Оба поля должны содержать числа')
-    result = x1 + x2
-    return render_template('lab4/sum.html', x1=x1, x2=x2, result=result)
+MAX_TREES = 10
+@lab4.route('/lab4/tree', methods = ['GET', 'POST'])
+def tree():
+    global tree_count
+    if request.method == 'GET':
+        return render_template('lab4/tree.html', tree_count=tree_count)
+    
+    operation = request.form.get('operation')
 
-# Умножение
-@lab4.route('/lab4/mul-form')
-def mul_form():
-    return render_template('lab4/mul-form.html')
+    if operation == 'cut':
+        if tree_count > 0:
+            tree_count -= 1
+    elif operation == 'plant':
+        if tree_count < MAX_TREES:
+            tree_count += 1
+    
 
-@lab4.route('/lab4/mul', methods=['POST'])
-def mul():
-    x1 = request.form.get('x1') or '1'
-    x2 = request.form.get('x2') or '1'
-    try:
-        x1 = int(x1)
-        x2 = int(x2)
-    except ValueError:
-        return render_template('lab4/mul.html', error='Оба поля должны содержать числа')
-    result = x1 * x2
-    return render_template('lab4/mul.html', x1=x1, x2=x2, result=result)
-
-# Вычитание
-@lab4.route('/lab4/sub-form')
-def sub_form():
-    return render_template('lab4/sub-form.html')
-
-@lab4.route('/lab4/sub', methods=['POST'])
-def sub():
-    x1 = request.form.get('x1')
-    x2 = request.form.get('x2')
-    if x1 == '' or x2 == '':
-        return render_template('lab4/sub.html', error='Оба поля должны содержать числа')
-    try:
-        x1 = int(x1)
-        x2 = int(x2)
-    except ValueError:
-        return render_template('lab4/sub.html', error='Оба поля должны содержать числа')
-    result = x1 - x2
-    return render_template('lab4/sub.html', x1=x1, x2=x2, result=result)
-
-# Возведение в степень
-@lab4.route('/lab4/pow-form')
-def pow_form():
-    return render_template('lab4/pow-form.html')
-
-@lab4.route('/lab4/pow', methods=['POST'])
-def pow_():
-    x1 = request.form.get('x1')
-    x2 = request.form.get('x2')
-    if x1 == '' or x2 == '':
-        return render_template('lab4/pow.html', error='Оба поля должны содержать числа')
-    try:
-        x1 = int(x1)
-        x2 = int(x2)
-    except ValueError:
-        return render_template('lab4/pow.html', error='Введите числовые значения')
-    if x1 == 0 and x2 == 0:
-        return render_template('lab4/pow.html', error='0^0 не определено')
-    result = x1 ** x2
-    return render_template('lab4/pow.html', x1=x1, x2=x2, result=result)
+    return redirect('/lab4/tree')
