feb90ddb6de982c3edc512e0c8abf62052182ad1
Лапшин Егор <egorkafghj@gmail.com>
2024-11-30 13:10:28 +0700

    Заготовка к 7-ой лабораторной

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 from dotenv import load_dotenv
 
 app = Flask(__name__)
@@ -19,6 +20,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 @app.route("/")
 @app.route("/index")
 def start():
@@ -48,6 +50,7 @@ def menu():
         <li><a href="/lab4">Лабораторная 4</a></li>
         <li><a href="/lab5">Лабораторная 5</a></li>
         <li><a href="/lab6">Лабораторная 6</a></li>
+        <li><a href="/lab7">Лабораторная 7</a></li>
         </div>
         <footer>
             &copy; Егор Лапшин , ФБИ-23, 3 курс, 2024
