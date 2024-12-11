e2bf5a4783f2b01eb7922d230094e110e10c5d0c
Лапшин Егор <egorkafghj@gmail.com>
2024-12-11 16:13:31 +0700

    редактирование фильма

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -19,6 +19,9 @@ function fillFilmList(){
 
             let editButton = document.createElement('button');
             editButton.innerText = 'редактировать';
+            editButton.onclick = function(){
+                editFilm(i);
+            }
 
             let delButton = document.createElement('button');
             delButton.innerText = 'удалить';
@@ -57,14 +60,15 @@ function cancel(){
     hideModal();
 }
 function addFilm(){
-    // document.getElementById('id').value = '';
-    // document.getElementById('title').value = '';
-    // document.getElementById('title-ru').value = '';
-    // document.getElementById('year').value = '';
-    // document.getElementById('description').value = '';
+    document.getElementById('id').value = '';
+    document.getElementById('title').value = '';
+    document.getElementById('title-ru').value = '';
+    document.getElementById('year').value = '';
+    document.getElementById('description').value = '';
     showModal();
 }
 function sendFilm(){
+    const id = document.getElementById('id').value;
     const film = {
         title: document.getElementById('title').value,
         title_ru: document.getElementById('title-ru').value,
@@ -72,8 +76,8 @@ function sendFilm(){
         description: document.getElementById('description').value
     }
 
-    const url = '/lab7/rest-api/films/';
-    const method = 'POST';
+    const url = `/lab7/rest-api/films/${id}`;
+    const method = id === ''? 'POST':'PUT';
 
     fetch(url,{
         method:method,  
@@ -83,5 +87,20 @@ function sendFilm(){
     .then(function(){
         fillFilmList();
         hideModal();
+    });
+}
+
+function editFilm(id){
+    fetch(`/lab7/rest-api/films/${id}`)
+    .then(function(data){
+        return data.json();
+    })
+    .then(function(film){
+        document.getElementById('id').value = id;
+        document.getElementById('title').value = film.title;
+        document.getElementById('title-ru').value = film.title_ru;
+        document.getElementById('year').value = film.year;
+        document.getElementById('description').value = film.description;
+        showModal();
     });
 }
